5e277a0ad2f914c9dc10fa8eab5cfc5412271146
AhmadjonM <157060690+AhmadjonM@users.noreply.github.com>
2024-10-24 00:19:37 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+var fruits = ["bannana", "orange","apple"];
+console.log(fruits[1]);
+
+
+
+var cars = ["BMW", false,"Volkswagen","Mersedes"];
+
+var cars2 = cars;
+cars2.push("Mitsubishi");
+
+console.log(cars);
+
+let i = 0;
+while (count < cars.length) {
+        cars[i] = i;
+
+    i++;
+}
+
+console.log(cars);
